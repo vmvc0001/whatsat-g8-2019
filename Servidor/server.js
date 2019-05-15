8e0340df56b1d7c08f300c744c2cf93905e90e32
vmvc0001 <48995419+vmvc0001@users.noreply.github.com>
2019-05-15 16:32:21 +0200

    xcvxc

--- Servidor/server.js
+++ Servidor/server.js
@@ -1,6 +1,42 @@
-var express = require('express');
-var app = express();
-app.get("/", function(req, res) {
-res.send('Sever SSAATT running...');
+var model=require("./model.js");
+var express=require("express");
+var http=require("http");
+var app=express();
+app.set("view engine","ejs");
+
+var bodyParser=require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+// app.get("/",function(res,req){
+//   res.render("index");
+// });
+
+app.get("/contactos/:id",function(res,req){
+  var dao=new model.DaoContactos();
+  dao.read(req.params.id).then(dtoContactos => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Content-Type', 'application/json');
+    res.render("contactos",{"listaContactos":dtoContactos});
+  });
 });
-app.listen(23700);
+
+app.post("/envia",function(res,req){
+  var dao=new model.DaoMensajes();
+  var dtoMensajes=req.body;
+  dao.create(dtoMensajes);
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Content-Type', 'application/json');
+    res.render("envia",{"status":"Status: ok"});
+});
+
+app.get("/recibe/:id_org/:id_dest",function (req,res) {
+  var dao=new model.DaoMensajes();
+  dao.read(req.params.id_org,req.params.id_dest).then(dtoMensajes => {
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Content-Type', 'application/json');
+    res.render("recibe",{"mensaje":dtoMensajes});
+  });
+});
+
+http.createServer(app).listen(80);
