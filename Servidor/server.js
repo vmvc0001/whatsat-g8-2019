ffa47a82ea9a70c38002c5e4fcfecfd847c48388
vmvc0001 <48995419+vmvc0001@users.noreply.github.com>
2019-05-18 11:17:09 +0200

    Final del tó

--- Servidor/server.js
+++ Servidor/server.js
@@ -1,13 +1,12 @@
+var bodyParser=require("body-parser");
 var model=require("./model.js");
 var express=require("express");
 var http=require("http");
 var app=express();
-app.set("view engine","ejs");
-app.use(express.static(__dirname +'/views')); //permite acceder a los recursos estaticos del directorio views como las imagenes y el css
-
-var bodyParser=require("body-parser");
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(express.static(__dirname +'/views')); //permite acceder a los recursos estaticos del directorio views como las imagenes y el css
+app.set("view engine","ejs");
 
 app.get("/",function(req,res){
    res.render("index");
@@ -26,6 +25,8 @@ app.get("/contactos/:id",function(req,res){
 app.post("/envia",function(req,res){
   var dao=new model.DaoMensajes();
   var dtoMensajes=req.body;
+  //var dtoMensajes={"id_org":req.body.id_org,"id_dest":req.body.id_dest,"tipo":req.body.tipo,"contenido":req.body.contenido};
+  console.log(dtoMensajes);
   dao.create(dtoMensajes);
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Content-Type', 'application/json');
@@ -39,6 +40,10 @@ app.get("/recibe/:id_org/:id_dest",function (req,res) {
     res.setHeader('Content-Type', 'application/json');
     res.render("recibe",{"mensaje":dtoMensajes});
   });
+});
+
+app.get("/test",function(req,res){
+	res.render("test");
 });
 
 http.createServer(app).listen(6969);
